e6688ba07d117b2978ca7065bd126724fe64be52
Alexey Melnikov <alexey.ernest@gmail.com>
2015-08-25 18:09:29 +0300

    Get public IP from metadata

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PUBLIC_IP_ADDRESS=$( curl http://ip4.me 2>/dev/null | sed -e 's#<[^>]*>##g' | grep '^[0-9]' )
+PUBLIC_IP_ADDRESS=$( curl http://169.254.169.254/latest/meta-data/public-ipv4 )
 echo "BROADCAST ADDRESS: $PUBLIC_IP_ADDRESS"
 
 exec /gopath/src/github.com/bitly/nsq/apps/nsqd/nsqd "$@" "--broadcast-address=$PUBLIC_IP_ADDRESS"
