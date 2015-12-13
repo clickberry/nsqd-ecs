9fc410448dc5f51e17f70e052099e66bbb60e325
Alexey Melnikov <alexey.ernest@gmail.com>
2015-12-13 20:30:00 +0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@
 PUBLIC_IP_ADDRESS=$( curl http://ip4.me 2>/dev/null | sed -e 's#<[^>]*>##g' | grep '^[0-9]' )
 echo "BROADCAST ADDRESS: $PUBLIC_IP_ADDRESS"
 
-exec /gopath/src/github.com/bitly/nsq/apps/nsqd/nsqd "$@" "--broadcast-address=$PUBLIC_IP_ADDRESS"
+exec /nsqd "$@" "--broadcast-address=$PUBLIC_IP_ADDRESS"
