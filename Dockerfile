913af977c0fa6897d1d7f3c85480251c7c18fce4
Alexey Melnikov <alexey.ernest@gmail.com>
2015-12-14 16:22:37 +0600

    Fix Entrypoint for Busybox

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM nsqio/nsq
 
 ADD ./docker-entrypoint.sh /
 RUN chmod +x ./docker-entrypoint.sh
-ENTRYPOINT ["./docker-entrypoint.sh", "--data-path=/data"]
+ENTRYPOINT ["/bin/sh", "./docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-PUBLIC_IP_ADDRESS=$( curl http://ip4.me 2>/dev/null | sed -e 's#<[^>]*>##g' | grep '^[0-9]' )
+PUBLIC_IP_ADDRESS=$( wget -O - http://ip4.me 2>/dev/null | sed -e 's#<[^>]*>##g' | grep '^[0-9]' )
 echo "BROADCAST ADDRESS: $PUBLIC_IP_ADDRESS"
 
-exec /nsqd "--broadcast-address=$PUBLIC_IP_ADDRESS"
+exec nsqd "$@" "--data-path=/data" "--broadcast-address=$PUBLIC_IP_ADDRESS"
